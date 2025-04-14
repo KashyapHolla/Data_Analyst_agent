5c7cdd15cdda942446734e1003dce44fafd4468b
kaushik-holla <holla.kaushik25@gmail.com>
2025-04-14 14:49:55 -0400

    Remove deleted files

--- app/nodes/status_updater_node.py
+++
@@ -1,6 +0,0 @@
-def state_updater_node(state: dict) -> dict:
-    """
-    A passthrough node to cleanly update and return the state.
-    Useful between tool execution and looping back to dispatcher.
-    """
-    return state
